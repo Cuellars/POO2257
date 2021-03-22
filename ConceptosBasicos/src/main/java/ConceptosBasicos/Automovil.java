3c256b8eb468ebd030aa0ff904f168b41e69395e
Cuellar Salinas <72581321+Cuellars@users.noreply.github.com>
2021-03-21 23:59:11 -0600

    Add files via upload

---
+++ ConceptosBasicos/src/main/java/ConceptosBasicos/Automovil.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConceptosBasicos;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author CUELLAR
+ */
+class Automovil {
+
+    void setMarca(String acura_Honda) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setSubMarca(String supercarro_NSX) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setModelo(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setColor(Color yellow) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

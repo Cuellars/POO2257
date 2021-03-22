1d9b5e8961865acd46f644e4c9440e859fa78a20
Cuellar Salinas <72581321+Cuellars@users.noreply.github.com>
2021-03-22 00:02:02 -0600

    Add files via upload

--- ConceptosBasicos/src/main/java/ConceptosBasicos/Automovil.java
+++ ConceptosBasicos/src/main/java/ConceptosBasicos/Automovil.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ConceptosBasicos;
 
 import java.awt.Color;
 
-/**
- *
- * @author CUELLAR
- */
+
 class Automovil {
 
     void setMarca(String acura_Honda) {
